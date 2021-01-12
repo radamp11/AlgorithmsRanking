da2760affec7c96c666e6fd0be2eb93c41bc9f9e
radamp11 <adamstec11@gmail.com>
2021-01-12 17:54:59 +0100

    radampcommit

--- website/algorithms/views.py
+++ website/algorithms/views.py
@@ -20,11 +20,31 @@ class DetailView(DetailView):
     model = Algorithm
     template_name = 'algorithms/detail.html'
 
+    def get_context_data(self, **kwargs):
+        alg = super().get_object()
+        context = super().get_context_data(**kwargs)
+        err_message = None
+        if not alg.counted:
+            try:
+                error = szkielet.generateResults("media/" + str(alg.alg_file))
+            except RuntimeError as ex:
+                err_message = ex.args
+            context['err_message'] = err_message
+
+        if err_message != None:
+            alg.delete()
+        return context
+
     def get_object(self):
         alg = super().get_object()
 
         if not alg.counted:
-            dims = szkielet.generateResults("media/" + str(alg.alg_file))
+            try:
+                dims = szkielet.generateResults("media/" + str(alg.alg_file))
+            except RuntimeError as ex:
+                #message = ex.args 
+                #print('wywaliło wyjatek')
+                return alg
             if dims.__len__() == 0:
                 alg.delete()
                 return alg
@@ -188,12 +208,15 @@ class CompAlgView(View):
                     try:
                         mean_val = szkielet.calculateGraph("media/" + file_name1, fun, dim, 0)
                     except RuntimeError as ex:
-                        return render(request, self.template_name, { 'form' : ex.args })
+                        return render(request, self.template_name, { 'form': form, 'error_cpp' : ex.args })
                 else:
-                    mean_val = szkielet.calculateGraph("media/" + file_name2, fun, dim, 0)
+                    try:
+                        mean_val = szkielet.calculateGraph("media/" + file_name2, fun, dim, 0)
+                    except RuntimeError as ex:
+                        return render(request, self.template_name, { 'form': form, 'error_cpp' : ex.args })
 
                 if mean_val.__len__() == 0:
-                    return render(request, self.template_name, {'form': "Blad przy pobieraniu wynikow z archiwum"})
+                    return render(request, self.template_name, {'form': form, 'error_cpp': "Blad przy pobieraniu wynikow z archiwum"})
 
                 # x_axis = [0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
                 x_axis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -218,9 +241,15 @@ class CompAlgView(View):
             fig_html = mpld3.fig_to_html(fig)
 
             plt.clf()
-
-            data_alg_1 = szkielet.calculateGraph("media/" + file_name1, fun, dim, 1)
-            data_alg_2 = szkielet.calculateGraph("media/" + file_name2, fun, dim, 1)
+            try:
+                data_alg_1 = szkielet.calculateGraph("media/" + file_name1, fun, dim, 1)
+            except RuntimeError as ex:
+                return render(request, self.template_name, { 'form': form, 'error_cpp' : ex.args })
+            try:
+                data_alg_2 = szkielet.calculateGraph("media/" + file_name2, fun, dim, 1)
+            except RuntimeError as ex:
+                return render(request, self.template_name, { 'form': form, 'error_cpp' : ex.args })
+            
             data_boxplot = [data_alg_1, data_alg_2]
             plt.title("Boxplot")
             plt.boxplot(data_boxplot, positions=[0.75, 1.25], labels=[alg1.name, alg2.name])
@@ -245,14 +274,19 @@ class RankingView(ListView):
     template_name = 'algorithms/ranking.html'
 
     def get_queryset(self):
-        min_SE = Algorithm.objects.all().aggregate(Min('SE'))
-        print(min_SE['SE__min'])
-        for alg in Algorithm.objects.all():
-            print(alg.SE)
-            alg.score = ((1 - (alg.SE - min_SE['SE__min']) / (alg.SE)) * 50)
-            alg.save()
 
-        return Algorithm.objects.order_by('score')
+        min_SE_2017 = Algorithm.objects.filter(num_of_fun = 30).aggregate(Min('SE'))
+        min_SE_2013 = Algorithm.objects.filter(num_of_fun = 28).aggregate(Min('SE'))
+        print(min_SE_2017['SE__min'])
+        print(min_SE_2013['SE__min'])
+        for alg in Algorithm.objects.all():
+            if alg.num_of_fun == 30:
+                alg.score1 = ((1 - (alg.SE - min_SE_2017['SE__min']) / (alg.SE)) * 50) 
+            elif alg.num_of_fun == 28:
+                alg.score1 = ((1 - (alg.SE - min_SE_2013['SE__min']) / (alg.SE)) * 50)
+            alg.total_score = alg.score1 + alg.score2
+            alg.save()
+        return Algorithm.objects.order_by('-total_score')
 
         # 123zpr123
 
@@ -273,9 +307,4 @@ class BenchmarksView(View):
             name = bench.name
             file_path = bench.data_file_path
 
-<<<<<<< HEAD
-        return render(request, self.template_name, {'form': form, 'name': name, 'file_path': file_path})
-=======
-
         return render(request, self.template_name, { 'form' : form, 'name': name, 'file_path': file_path })
->>>>>>> 8e8d270224f3414887fd9feff18c491f5af73586
